9cc4c16ecfe85d99380fddddf201ea843b90d642
renaldo20 <renotrash.beekman@gmail.com>
2016-02-08 17:05:34 +0100

    Use new corrected data

--- lightningcoin.py
+++ lightningcoin.py
@@ -1,9 +1,9 @@
 import tables
 import sapphire
 
-data = tables.open_file('/Users/reno/HiSPARC/Code/lightning/lightning_10km.h5', 'r')
+data = tables.open_file('/Users/reno/HiSPARC/Code/lightning/lightning_10km_120sec.h5', 'r')
 
-timestamps = data.root.lightning.timestamps
+timestamps = data.root.lightning.corr_timestamps
 nanoseconds = data.root.lightning.nanoseconds
 n_stations = data.root.lightning.n_stations
 stations = data.root.lightning.stations
@@ -14,21 +14,26 @@ dts = [] # lege lijst met tijdverschillen dt
 
 ndts = 0
 
-for i in sapphire.utils.pbar(xrange(nrows)):
+for i in sapphire.utils.pbar(xrange(nrows), show=False):
     statname = ['/s%s' % u for u in stations[i].split(',')]
     litime = timestamps[i]
-    linano = nanoseconds[i]*1e-9
+    linano = nanoseconds[i] * 1e-9
     for j in xrange(len(statname)):
         ev = data.get_node(statname[j], 'events')
         evtimecol = ev.col('timestamp')
-        nanoseccol = ev.col('nanoseconds')*1e-9
+        nanoseccol = ev.col('nanoseconds') * 1e-9
         diftime = evtimecol - litime + nanoseccol - linano
 
-        filter14 = (abs(diftime + 14) < 0.0002)
-        filter13 = (abs(diftime + 13) < 0.0002)
+#         filter14 = (abs(diftime + 14) < 0.0002)
+#         filter13 = (abs(diftime + 13) < 0.0002)
         filter0 = (abs(diftime) < 0.0002)
-        filters = filter14 & filter13 & filter0
-        ev.read_where(filters)
+#         filters = filter14 & filter13 & filter0
 
-        dt = diftime.compress(filters)
-        dts.extend(dt)
+        #nu eerst even alleen rond dt=0
+        events = ev[filter0]
+        if len(events):
+            print i, statname[j]
+            print events
+
+#        dt = diftime.compress(filters)
+#        dts.extend(dt)
